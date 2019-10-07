15b7d2349e5d87c97c0197a1a5e62e2ef778b93a
daraahh <pikachu.dario@gmail.com>
2019-10-07 13:01:48 +0200

    Añado test para #11. Desarrolla #6

--- t/unit_test.rb
+++ t/unit_test.rb
@@ -1,14 +1,25 @@
 
-require 'test-unit'
+require 'test/unit'
 require 'json'
 require_relative '../src/SchedManager.rb'
 
 class TestProyecto < Test::Unit::TestCase
 
-	def test_get_from_id
+	setup do
 		@manager = SchedManager.new
+	end
+
+	def test_get_from_id
 		recovered = @manager.getAsignatura("110")
 		assert_equal("110",recovered["id"],"elemento recuperado es el esperado")
+	end
+
+	def test_add_asignatura
+		id_to_add = "113"
+		to_add = {"id"=>id_to_add, "nombre"=>"Asignatura test", "Grupo"=>"F", "teoria"=>"J-9:30", "practicas"=>["M-11:30", "J-11:30", "V-11:30"]}
+		@manager.addAsignatura(to_add)
+		recovered = @manager.getAsignatura(id_to_add)
+		assert_equal(id_to_add,recovered["id"],"elemento recuperado es el añadido")
 	end
 
 end
