67a4ca1247532b6dad64bc54f2bc9228392cd08a
daraahh <pikachu.dario@gmail.com>
2019-10-07 17:24:33 +0200

    Implementado eliminar asignatura. Closes #12

--- src/SchedManager.rb
+++ src/SchedManager.rb
@@ -5,21 +5,32 @@ class SchedManager
 	def initialize
 		@path = File.join(File.dirname(__FILE__),'../sampledata/sampledata.json')
 		@file = File.read(@path)
-		@parseddata = JSON.parse(@file)
+		@data = JSON.parse(@file)
+	end
+
+	def write2file()
+		File.open(@path, "w") do |f|
+			f.puts JSON.pretty_generate(@data)
+		end
 	end
 
 	def getAsignatura(id)
-		@parseddata["asignaturas"].each do |a|
+		@data["asignaturas"].each do |a|
 			return asignatura = a if (a["id"] == id)
 		end
 		nil
 	end
 
 	def addAsignatura(info)
-		@parseddata["asignaturas"] << info
-		File.open(@path, "w") do |f|
-			f.puts JSON.pretty_generate(@parseddata)
+		@data["asignaturas"] << info
+		write2file()
+	end
+
+	def removeAsignatura(id)
+		@data["asignaturas"].each do |a|
+			@data["asignaturas"].delete(a) if (a["id"] == id)
 		end
+		write2file()
 	end
 
 end
