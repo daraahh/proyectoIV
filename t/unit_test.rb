e7921388d71fc3bb32f431c8a3111ce5baef9e40
daraahh <pikachu.dario@gmail.com>
2019-10-07 18:31:08 +0200

    Modifico test para #19. Desarrolla #6

--- t/unit_test.rb
+++ t/unit_test.rb
@@ -10,12 +10,13 @@ class TestProyecto < Test::Unit::TestCase
 	end
 
 	def test_get_from_id
-		recovered = @manager.getAsignatura("110")
-		assert_equal("110",recovered["id"],"elemento recuperado es el esperado")
+		expected = {"nombre"=>"Fundamentos de programación", "Grupo"=>"B", "teoria"=>"J-9:30", "practicas"=>["M-11:30", "J-11:30", "V-11:30"]}
+		recovered = @manager.getAsignatura("1100")
+		assert_equal(expected,recovered,"elemento recuperado es el esperado")
 	end
 
 	def test_add_asignatura
-		id_to_add = "113"
+		id_to_add = "1103"
 		to_add = {"id"=>id_to_add, "nombre"=>"Asignatura test", "Grupo"=>"F", "teoria"=>"J-9:30", "practicas"=>["M-11:30", "J-11:30", "V-11:30"]}
 		@manager.addAsignatura(to_add)
 		recovered = @manager.getAsignatura(id_to_add)
@@ -23,7 +24,7 @@ class TestProyecto < Test::Unit::TestCase
 	end
 
 	def test_remove_asignatura
-		to_remove = "113"
+		to_remove = "1103"
 		@manager.removeAsignatura(to_remove)
 		recovered = @manager.getAsignatura(to_remove)
 		assert_equal(nil,recovered,"elemento ha sido eliminado")
