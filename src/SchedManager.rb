9209d0af163e7a60f3c3fba2195058ca09377f7d
daraahh <pikachu.dario@gmail.com>
2019-10-07 13:01:00 +0200

    Implementado añadir una asignatura. Closes #11

--- src/SchedManager.rb
+++ src/SchedManager.rb
@@ -3,8 +3,9 @@ require 'json'
 class SchedManager
 
 	def initialize
-		path = File.join(File.dirname(__FILE__),'../sampledata/sampledata.json')
-		@parseddata = JSON.parse(File.read(path))
+		@path = File.join(File.dirname(__FILE__),'../sampledata/sampledata.json')
+		@file = File.read(@path)
+		@parseddata = JSON.parse(@file)
 	end
 
 	def getAsignatura(id)
@@ -14,5 +15,11 @@ class SchedManager
 		nil
 	end
 
+	def addAsignatura(info)
+		@parseddata["asignaturas"] << info
+		File.open(@path, "w") do |f|
+			f.puts JSON.pretty_generate(@parseddata)
+		end
+	end
 
 end
