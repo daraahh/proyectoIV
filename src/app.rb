6ba6dbd01bd0009f58733157a4243a2f8828f25e
daraahh <pikachu.dario@gmail.com>
2019-10-27 17:07:23 +0100

    Añado ruta para recuperar asignatura mediante id. Closes #35

--- src/app.rb
+++ src/app.rb
@@ -4,6 +4,10 @@ require_relative 'SchedManager.rb'
 
 class App < Sinatra::Base
 
+	before do
+		@manager = SchedManager.new
+	end
+
 	get '/' do
 		content_type :json
 		{:status => 'OK'}.to_json
@@ -13,4 +17,16 @@ class App < Sinatra::Base
 		content_type :json
 		{:status => 'OK'}.to_json
 	end
+
+	get '/asignatura/:id' do |id|
+		content_type :json
+		resultado = @manager.getAsignatura(id)
+		if resultado == nil
+			status 404
+			{:status => 'Error: Asignatura no encontrada.'}.to_json
+		else
+			resultado.to_json
+		end
+	end
+
 end
